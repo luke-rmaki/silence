f967435fc0d756315f840d5d614902f2ecdda8af
Luke <luke@rmaki.com.au>
2022-07-16 15:24:06 +1000

    add color list

---
+++ src/routes/color-list.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { colors } from '../lib/colors';
+
+	const colorNames = Object.keys(colors);
+
+	const list = colorNames
+		.map((name) => {
+			return colors[name].map((color, index) => {
+				const varName = `--${name}-${index}`;
+				const value = `${color}`;
+				return `${varName}: ${value};`;
+			});
+		})
+		.flat();
+</script>
+
+<ul>
+	{#each list as item}
+		<li>{item}</li>
+	{/each}
+</ul>
+
+<style lang="scss">
+	ul {
+		list-style: none;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 
 <div>
 	<a href="/colors">Colors</a>
+	<a href="/color-list">Color List</a>
 </div>
 
 <style lang="scss">
